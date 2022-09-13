28d487ad2e9355094dffe904c849d6532cfe7fbd
NAMARAMA88 <coulrama2@gmail.com>
2022-09-13 09:48:23 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+nclude "main.h"
+
+/**
+* print_alphabet_x10 - 10 times alphabe
+*/
+
+void print_alphabet_x10(void)
+{
+	int y = 0;
+
+	while (y < 10)
+{
+		int s;
+
+		for (s = 'a'; s <= 'z'; s++)
+			_putchar(s);
+		_putchar('\n');
+		y++;
+}
+}
