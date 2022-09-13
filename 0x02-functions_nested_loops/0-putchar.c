87ae8e3b74ce917cc7a7e75dfdfb318c07bf7129
NAMARAMA88 <coulrama2@gmail.com>
2022-09-13 08:22:47 -0700

    readme added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - prints put
- *  Return: always 0
- */
+* main - prints put
+*  Return: always 0
+*/
 int main(void)
 {
 	_putchar('_');
@@ -17,4 +17,5 @@ int main(void)
 	_putchar('\n');
 
 	return (0);
+
 }
