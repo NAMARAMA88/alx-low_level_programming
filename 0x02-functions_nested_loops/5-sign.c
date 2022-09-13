abac2c5dd4facd190a9a242047ff657464f6141f
NAMARAMA88 <coulrama2@gmail.com>
2022-09-13 12:42:28 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,30 +1,29 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_sign - check numers
- * @n : number to check
- * Return:0 or 1
+ * print_sign - print the sign of the input
+ * @n: the variable to be checked
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
  */
 
 int print_sign(int n)
 {
-int test;
 
-if (n > 0)
-{
-	test = 1;
-	_putchar('+');
-}
-else if (n == 0)
-{
-	test = 0;
-	_putchar('0');
-}
-else
-{
-	test = -1;
-	_putchar('-');
-}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
 
-return (test);
 }
