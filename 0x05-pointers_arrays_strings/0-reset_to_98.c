0a93fc77647239b63de844894ab12288924dc54f
NAMARAMA88 <coulrama2@gmail.com>
2022-09-19 03:29:47 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,10 @@
 #include "main.h"
-/**
- * reset_to_98 - reset argument to 98
- * @n: pointer
- * Return: void
-*/
 
+/**
+ * eset_to_98 -> a function to update the reference of a pointer
+ * @n: pointer to n to be updated
+ */
 void reset_to_98(int *n)
 {
-
-*n = 98;
-
+	*n = 98;
 }
