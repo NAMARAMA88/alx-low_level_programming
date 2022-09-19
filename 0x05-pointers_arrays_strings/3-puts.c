86b932448f349d3a78648907bffea725d405e5f4
NAMARAMA88 <coulrama2@gmail.com>
2022-09-19 09:24:24 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,7 +8,7 @@
  * Description: prints a string
  * On success: returns no error
  */
-void puts(char *str)
+void _puts(char *str)
 {
 	int i = 0;
 
@@ -19,4 +19,3 @@ void puts(char *str)
 	}
 	putchar(10);
 }
-
