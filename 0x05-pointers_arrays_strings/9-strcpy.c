5510b207780a376c0791a87a7205709230d25ca4
NAMARAMA88 <coulrama2@gmail.com>
2022-09-20 15:27:18 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * strcpy - Copy paste string
- * @dest: destination
- * @src: source
- *
- * Return: destination
- */
+* _strcpy - Copy paste string
+* @dest: destination
+* @src: source
+*
+* Return: destination
+*/
 
 char *_strcpy(char *dest, char *src)
 {
