bdbb58d2e92efdc721696136eae7e094d30a2765
NAMARAMA88 <coulrama2@gmail.com>
2022-09-20 09:45:02 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,3 +16,13 @@ void print_rev(char *s)
 		putchar ((*s + len));
 	putchar(10);
 }
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+}
